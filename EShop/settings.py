e240c8eb3f1d4617df2ced1a4019505dba8e9faa
Gessa Lutfhan Waldhani <gieshalutfian@gmail.com>
2024-12-18 11:48:53 +0700

    Template add

--- EShop/settings.py
+++ EShop/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'EShop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -132,13 +132,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_DITS = [BASE_DIR /'static']
 
-MEDIA_URL = "/media/" # website.com/media/
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+MEDIA_URL = "/media/" 
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
--- store/views.py
+++ store/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse("HELLO WORLD")
+    return render(request, "store/index.html")  
